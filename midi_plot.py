89b13a6cdea913f2a3bcb317e3b7e00e8094f72b
HaowenWeiJohn <59973823+HaowenWeiJohn@users.noreply.github.com>
2026-03-13 15:11:50 -0400

    test duration

--- midi_plot.py
+++ midi_plot.py
@@ -7,7 +7,10 @@ import midi
 midi_file_path = r"\\192.168.1.104\home\piano\data\045\disklavier\20250815_144525_pia02_s045_007_tempo_ramp_ap345.mid"
 midi_log = midi.Log(midi_file_path)
 signals = midi_log.to_signals(pedal_threshold=None)()
+print(midi_log.duration)
+print(midi_log.pm.get_end_time())
+
 print(signals.shape)
 print(signals.shape[0]/midi_log.sr)
-print(midi_log.duration)
+
 notes = midi_log.notes
